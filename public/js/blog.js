c3aa4682e251e2a214d7988c3d628e3f034b0d87
Justin Dietert <justin.dietert@gmail.com>
2015-02-19 15:16:26 -0600

    making sense of blog posting with js

--- public/js/blog.js
+++ public/js/blog.js
@@ -11,7 +11,7 @@ var blog = [
             "CSS"
         ],
         "category": "Design",
-        "post": "lorem ipsum dolor and stuff here"
+        "body": "Bacon ipsum dolor amet culpa proident et tri-tip quis fatback, eu venison short ribs sirloin salami nulla hamburger kevin excepteur. Short loin nulla cow tri-tip commodo. Flank capicola jerky, cillum commodo quis fugiat ea tenderloin ham consectetur. Boudin aliqua adipisicing rump corned beef. Nulla corned beef sunt ball tip, qui bresaola enim jowl. Capicola short ribs minim salami nulla nostrud pastrami. Pork drumstick turkey fugiat. Tri-tip elit turducken pork chop in. Swine short ribs meatball irure bacon nulla pork belly cupidatat meatloaf cow. Nulla corned beef sunt ball tip, qui bresaola enim jowl. Capicola short ribs minim salami nulla nostrud pastrami. Nulla corned beef sunt ball tip, qui bresaola enim jowl. Capicola short ribs minim salami nulla nostrud pastrami. Pork drumstick turkey fugiat. Tri-tip elit turducken pork chop in. Swine short ribs meatball irure bacon nulla pork belly cupidatat meatloaf cow. Nulla corned beef sunt ball tip, qui bresaola enim jowl. Capicola short ribs minim salami nulla nostrud pastrami."
     },
     {
         "title": "Another title here",
@@ -19,23 +19,71 @@ var blog = [
         "author": "Justin Dietert",
         "image": "http://lorempixel.com/600/350/abstract/10/",
         "tags": [
-            "Web",
-            "Design",
-            "HTML",
+            "Bacon",
+            "Circuits",
+            "Resolution",
             "Media queries"
         ],
         "category": "Development",
-        "post": "lorem ipsum dolor and stuff here"
+        "body": "Bacon ipsum dolor amet culpa proident et tri-tip quis fatback, eu venison short ribs sirloin salami nulla hamburger kevin excepteur. Short loin nulla cow tri-tip commodo. Flank capicola jerky, cillum commodo quis fugiat ea tenderloin ham consectetur. Boudin aliqua adipisicing rump corned beef. Nulla corned beef sunt ball tip, qui bresaola enim jowl. Capicola short ribs minim salami nulla nostrud pastrami. Pork drumstick turkey fugiat. Tri-tip elit turducken pork chop in. Swine short ribs meatball irure bacon nulla pork belly cupidatat meatloaf cow. Nulla corned beef sunt ball tip, qui bresaola enim jowl. Capicola short ribs minim salami nulla nostrud pastrami. Nulla corned beef sunt ball tip, qui bresaola enim jowl. Capicola short ribs minim salami nulla nostrud pastrami. Pork drumstick turkey fugiat. Tri-tip elit turducken pork chop in. Swine short ribs meatball irure bacon nulla pork belly cupidatat meatloaf cow. Nulla corned beef sunt ball tip, qui bresaola enim jowl. Capicola short ribs minim salami nulla nostrud pastrami."
     }
 ]
 
-// title
-// date - author - category
-// tags: web, design, html
-// image - post
 
-// title
-// date - author - category
-// tags: web, design, html
-// post
-// image - post
+// ----------------------------------------------------
+
+// var postString = '';
+// var post = document.getElementById('blogPosts');
+
+
+// for (var i = 0; i < blog.length; i++) {
+
+//     var tags = '';
+//     for (var j = 0; j < blog[i].tags.length; j++) {
+//         tags += '<a href="#">' + blog[i].tags[j] + '</a>, ';
+//         //string split to take off last comma
+//     }
+   
+//     postString += '<article><hr /><h2><a href="#">' + blog[i].title + '</a></h2>';
+//     postString += '<h4>By <a href="#">' + blog[i].author + '</a></h4>';
+//     postString += '<h5>Posted on <a href="">' + blog[i].date + '</a> under <a href="">' + blog[i].category + '</a>.</h5>';
+//     postString += '<h5>Tags: ' + tags + '</h5>';
+//     postString += '<div class="row"><div class="medium-12 columns">';
+//     postString += '';
+
+//     postString += '</div></div></article>';
+// }
+
+// post.innerHTML = postString;
+
+// ----------------------------------------------------
+
+var allPosts = [];
+
+function populateExistingPost(post) {
+    var title = '<article><hr /><h2><a href="#">' + post.title + '</a></h2>';
+    var authorDate = '';
+    var tags = '';
+    post.tags.forEach(function (element, index, array) {
+        tags += element + ', ';
+    });
+
+    var body = '</article>';
+    var postHTML = title + authorDate + tags + body;
+    return postHTML;
+}
+
+
+
+blog.forEach(function (element, index, array) {
+    allPosts += populateExistingPost(element);
+});
+
+document.getElementById('blogPosts').innerHTML = allPosts;
+
+
+
+
+
+
+
