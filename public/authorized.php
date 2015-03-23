c4550533e9e21a41906ea60f46eff7361286c7c2
Justin Dietert <justin.dietert@gmail.com>
2015-03-23 11:10:48 -0500

    added POST exercise

---
+++ public/authorized.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Authorized</title>
+</head>
+<body>
+    <h3>AUTHORIZED</h3>
+</body>
+</html>
---
+++ public/login.php
@@ -0,0 +1,35 @@
+<?php
+    $username = isset($_POST['username']) ? $_POST['username'] : '';
+    $password = isset($_POST['password']) ? $_POST['password'] : '';
+
+    $message = '';
+
+    if ($username == 'guest' && $password == 'password') {
+        header('Location: authorized.php');
+    } else {
+        $message = 'Login failed.';
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+    <form method="POST" action="login.php">
+        <label for="username">Username: </label>
+        <input type="text" name="username"><br>
+
+        <label for="password">Password: </label>
+        <input type="password" name="password"><br>
+
+        <input type="submit">
+    </form>
+    
+    <? if ($_POST && strlen($message) > 1): ?>
+        <p><?= $message; ?></p>
+    <? endif; ?>
+
+</body>
+</html>
