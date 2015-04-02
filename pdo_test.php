dacc33e12273ac63ea8e833839dbac7868049ed5
Justin Dietert <justin.dietert@gmail.com>
2015-04-02 14:16:19 -0500

    added test file for PDO exercise

---
+++ pdo_test.php
@@ -0,0 +1,11 @@
+<?php
+
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'employees');
+define('DB_USER', 'codeup');
+define('DB_PASS', 'rocks');
+
+require_once 'db_connect.php';
+
+// echo the PDO connection status
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
