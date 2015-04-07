75130c991d59f4b6dbd37441cc0c9da4391f1f39
Justin Dietert <justin.dietert@gmail.com>
2015-04-07 09:57:07 -0500

    pulled out login credentials to an external php file

--- park_migration.php
+++ park_migration.php
@@ -1,10 +1,6 @@
 <?php
 
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'parks_db');
-define('DB_USER', 'parks_user');
-define('DB_PASS', 'rocks');
-
+require_once 'park_logins.php';
 require_once 'db_connect.php';
 
 $dbc->exec('DROP TABLE IF EXISTS national_parks');
