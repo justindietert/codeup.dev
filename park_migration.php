ebdd6c2b2ddf02c2bcfed53496f66bc2f18282ca
Justin Dietert <justin.dietert@gmail.com>
2015-04-08 15:35:43 -0500

    added description column to table

--- park_migration.php
+++ park_migration.php
@@ -6,13 +6,14 @@ require_once 'db_connect.php';
 $dbc->exec('DROP TABLE IF EXISTS national_parks');
 
 $query = 'CREATE TABLE national_parks (
-        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-        name VARCHAR(100) NOT NULL,
-        location VARCHAR(100) NOT NULL,
-        date_established DATE,
-        area_in_acres DOUBLE,
-        PRIMARY KEY (id)
-    )';
+            id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+            name VARCHAR(100) NOT NULL,
+            location VARCHAR(100) NOT NULL,
+            date_established DATE,
+            area_in_acres DOUBLE,
+            description TEXT,
+            PRIMARY KEY (id)
+          )';
 
 // Run query, if there are errors they will be thrown as PDOExceptions
 $dbc->exec($query);
