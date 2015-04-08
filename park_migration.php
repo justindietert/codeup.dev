89cc38896843112466bf3d97233bcce2dfd8e354
Justin Dietert <justin.dietert@gmail.com>
2015-04-08 15:39:24 -0500

    specified NOT NULL

--- park_migration.php
+++ park_migration.php
@@ -9,8 +9,8 @@ $query = 'CREATE TABLE national_parks (
             id INT UNSIGNED NOT NULL AUTO_INCREMENT,
             name VARCHAR(100) NOT NULL,
             location VARCHAR(100) NOT NULL,
-            date_established DATE,
-            area_in_acres DOUBLE,
+            date_established DATE NOT NULL,
+            area_in_acres DOUBLE NOT NULL,
             description TEXT,
             PRIMARY KEY (id)
           )';
