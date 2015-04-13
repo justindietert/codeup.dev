af772e75980a220cd00c52ab72005f922b3e90fb
Justin Dietert <justin.dietert@gmail.com>
2015-04-13 11:28:33 -0500

    completed __get and __set magic methods exercise

---
+++ Model.php
@@ -0,0 +1,35 @@
+<?php
+
+class Model 
+{
+    // array to store key/value data
+    private $attributes = [];
+
+    // magic setter to populate $attributes array
+    public function __set($name, $value)
+    {
+        // set the $name key to hold $value in $attributes
+        $this->attributes[$name] = $value;
+    }
+
+    // Magic getter to retrieve values from $attributes
+    public function __get($name)
+    {
+        // Check for existence of array key $name in $attributes array
+        if (array_key_exists($name, $this->attributes)) {
+            return $this->attributes[$name];
+        }
+
+        return null;
+    }
+}
+
+
+//------ TEST ------//
+
+$thing = new Model;
+
+$thing->fruit = 'orange';
+
+echo $thing->fruit . PHP_EOL;
+var_dump($thing->stuff);
