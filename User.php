f1140cb2d538a3e9d2cb9adccbd31677246092e4
Justin Dietert <justin.dietert@gmail.com>
2015-04-13 13:05:57 -0500

    completed exercise with Late Static Binding

--- User.php
+++ User.php
@@ -1,6 +1,12 @@
 <?php
 
+require_once 'Model.php';
+
 class User extends Model 
 {
-    
+    protected static $table = 'users';
 }
+
+//------ TEST ------//
+
+echo User::getTableName() . PHP_EOL;
