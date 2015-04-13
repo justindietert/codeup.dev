b73c48d00e5d7752144e9866ee150d60571ee892
Justin Dietert <justin.dietert@gmail.com>
2015-04-13 13:41:35 -0500

    replaced code with Model exercise code

--- Model.php
+++ Model.php
@@ -1,42 +1,95 @@
 <?php
 
-class Model 
-{
-    // array to store key/value data
-    private $attributes = [];
+class Model {
+
+    protected static $dbc;
     protected static $table;
 
-    // magic setter to populate $attributes array
-    public function __set($name, $value)
+    public $attributes = array();
+
+    /*
+     * Constructor
+     */
+    public function __construct()
     {
-        // set the $name key to hold $value in $attributes
-        $this->attributes[$name] = $value;
+        self::dbConnect();
     }
 
-    // Magic getter to retrieve values from $attributes
+    /*
+     * Connect to the DB
+     */
+    private static function dbConnect()
+    {
+        if (!self::$dbc)
+        {
+            // @TODO: Connect to database
+        }
+    }
+
+    /*
+     * Get a value from attributes based on name
+     */
     public function __get($name)
     {
-        // Check for existence of array key $name in $attributes array
-        if (array_key_exists($name, $this->attributes)) {
-            return $this->attributes[$name];
-        }
-
-        return null;
+        // @TODO: Return the value from attributes with a matching $name, if it exists
     }
 
-    public static function getTableName()
+    /*
+     * Set a new attribute for the object
+     */
+    public function __set($name, $value)
     {
-        return static::$table;
+        // @TODO: Store name/value pair in attributes array
+    }
+
+    /*
+     * Persist the object to the database
+     */
+    public function save()
+    {
+        // @TODO: Ensure there are attributes before attempting to save
+
+        // @TODO: Perform the proper action - if the `id` is set, this is an update, if not it is a insert
+
+        // @TODO: Ensure that update is properly handled with the id key
+
+        // @TODO: After insert, add the id back to the attributes array so the object can properly reflect the id
+
+        // @TODO: You will need to iterate through all the attributes to build the prepared query
+
+        // @TODO: Use prepared statements to ensure data security
+
+    /*
+     * Find a record based on an id
+     */
+    public static function find($id)
+    {
+        // Get connection to the database
+        self::dbConnect();
+
+        // @TODO: Create select statement using prepared statements
+
+        // @TODO: Store the resultset in a variable named $result
+
+        // The following code will set the attributes on the calling object based on the result variable's contents
+
+        $instance = null;
+        if ($result)
+        {
+            $instance = new static;
+            $instance->attributes = $result;
+        }
+        return $instance;
+    }
+
+    /*
+     * Find all records in a table
+     */
+    public static function all()
+    {
+        self::dbConnect();
+
+        // @TODO: Learning from the previous method, return all the matching records
     }
 
 }
-
-
-//------ TEST ------//
-
-$thing = new Model;
-
-$thing->fruit = 'orange';
-
-echo $thing->fruit . PHP_EOL;
-var_dump($thing->stuff);
